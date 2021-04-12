a5cd1e8c5ba1067f2ba914fcca5e1a5b62b12f6e
Borut Balazek <bobalazek124@gmail.com>
2021-04-12 17:31:14 +0200

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -86,6 +86,6 @@ module.exports = {
   output: {
     filename: 'static/js/[name].[fullhash:8].js',
     chunkFilename: 'static/js/[name].[fullhash:8].chunk.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: './dist',
   },
 };

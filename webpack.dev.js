2f9916a7e8b70757ebc0088bfa079b7bdf4b9659
Borut Balazek <bobalazek124@gmail.com>
2021-06-01 17:17:34 +0200

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'development',
